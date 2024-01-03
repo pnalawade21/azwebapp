c02e993247e4a5bf5b5e22751f9aab954bb65e9f
prashant nalawade <prashant.nalawade21@gmail.com>
2024-01-03 19:09:29 +0530

    Add project files.

---
+++ sqlapp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlapp.Models;
+using sqlapp.Services;
+
+namespace sqlapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> products;
+        public void OnGet()
+        {
+            var productService = new ProductService();
+
+            products = productService.GetProducts();
+        }
+    }
+}
